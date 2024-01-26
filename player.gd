09fdc5677053aff9e7adcdb11648ef0f78f0204c
Martin Riesz <riesz.martin@gmail.com>
2024-01-26 20:53:06 +0100

    Fix issues with zero gravity, walking on different slopes, jumping is broken

--- player.gd
+++ player.gd
@@ -19,7 +19,6 @@ func _ready() -> void:
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var delta := get_physics_process_delta_time()
 	var move_direction := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var global_direction := transform.basis * Vector3(move_direction.x, 0, move_direction.y)
 
 	var is_on_floor := false
 	var floor_velocity := Vector3.ZERO
@@ -29,23 +28,23 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 			if shape_owner_get_owner(state.get_contact_local_shape(i)) == feet_collision_shape:
 				is_on_floor = true
 				floor_velocity = state.get_contact_collider_velocity_at_position(i)
-				var upright_vector = -state.total_gravity.normalized()
-				state.transform.basis = state.transform.basis.slerp(Basis(
-					upright_vector.cross(-upright_vector.cross(state.transform.basis.x)),
-					upright_vector,
-					upright_vector.cross(-upright_vector.cross(state.transform.basis.z))
-				).orthonormalized(), 1 - pow(0.1, delta * SPEED))
+				if not state.total_gravity.is_zero_approx():
+					var upright_vector := -state.total_gravity.normalized()
+					var target_basis := Basis(
+						upright_vector.cross(-upright_vector.cross(state.transform.basis.x)),
+						upright_vector,
+						upright_vector.cross(-upright_vector.cross(state.transform.basis.z))
+					).orthonormalized()
+					state.transform.basis = state.transform.basis.slerp(target_basis, 1 - pow(0.1, delta * SPEED))
 				break
 
 	if is_on_floor:
-		state.linear_velocity.x = floor_velocity.x + global_direction.x * SPEED
-		state.linear_velocity.z = floor_velocity.z + global_direction.z * SPEED
-
-		# Handle jump.
+		var upward_velocity := 0.0
 		if Input.is_action_just_pressed("jump"):
-			state.linear_velocity.y = JUMP_VELOCITY
+			upward_velocity = JUMP_VELOCITY
+		state.linear_velocity = floor_velocity + transform.basis * Vector3(move_direction.x * SPEED, upward_velocity, move_direction.y * SPEED)
 
-	state.transform.basis = state.transform.rotated_local(Vector3(0, 1, 0), -look_direction_change.x).basis # rotate_y(-direction.x)
+	state.transform.basis = state.transform.rotated_local(Vector3.UP, -look_direction_change.x).basis # rotate_y(-direction.x)
 	neck.rotate_x(-look_direction_change.y)
 	neck.rotation.x = clampf(neck.rotation.x, TAU * -0.25, TAU * 0.25)
 	look_direction_change = Vector2.ZERO
