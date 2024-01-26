530c8b8c73aef2aa0bfcc72236a50f371b32050d
Martin Riesz <riesz.martin@gmail.com>
2024-01-26 02:40:47 +0100

    Test RigidBody3D player controller again with RigidBody3D platform with different collision masks

--- platform.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatableBody3D
-
-const SPEED = 5.0
-
-
-func _process(delta: float) -> void:
-	var velocity := Vector3.RIGHT * SPEED
-	global_position += velocity * delta
-	#move_and_collide(velocity * delta)
--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
-extends CharacterBody3D
+extends RigidBody3D
 
+@onready var ground_test: RayCast3D = $GroundTest
 @onready var camera := get_viewport().get_camera_3d()
 @onready var neck: Node3D = $Neck
 
@@ -8,41 +9,40 @@ const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.002
 const JOYSTICK_SENSITIVITY = 2.00
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 var look_direction_change = Vector2()
 
 
-func _process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var move_direction := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var global_direction := transform.basis * Vector3(move_direction.x, 0, move_direction.y)
 
 	if is_on_floor():
-		if global_direction:
-			velocity = Vector3(global_direction.x * SPEED, 0, global_direction.z * SPEED)
-		else:
-			velocity = velocity.move_toward(Vector3.ZERO, SPEED)
+		var platform_velocity := Vector3.ZERO
+		if ground_test.get_collider() is RigidBody3D:
+			platform_velocity = ground_test.get_collider().linear_velocity
+
+		state.linear_velocity.x = platform_velocity.x + global_direction.x * SPEED
+		state.linear_velocity.z = platform_velocity.z + global_direction.z * SPEED
 
 		# Handle jump.
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
+			state.linear_velocity.y = JUMP_VELOCITY
 
-	move_and_slide()
-
-
-	# Get the input direction and handle the looking around.
-	var look_dir := Input.get_vector("look_left", "look_right", "look_up", "look_down")
-	look_direction_change += look_dir * delta * JOYSTICK_SENSITIVITY
-
-	rotate_y(-look_direction_change.x)
+	state.transform.basis = state.transform.rotated_local(Vector3(0, 1, 0), -look_direction_change.x).basis # rotate_y(-direction.x)
 	neck.rotate_x(-look_direction_change.y)
 	neck.rotation.x = clampf(neck.rotation.x, TAU * -0.25, TAU * 0.25)
 	look_direction_change = Vector2.ZERO
+
+
+
+func is_on_floor():
+	return ground_test.is_colliding()
+
+
+func _process(delta: float) -> void:
+	# Get the input direction and handle the looking around.
+	var look_dir := Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	look_direction_change += look_dir * delta * JOYSTICK_SENSITIVITY
 
 
 func _input(event: InputEvent) -> void:
