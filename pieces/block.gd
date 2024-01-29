1a84a512dc88b9f8409456ef7221056e129baba6
Martin Riesz <riesz.martin@gmail.com>
2024-01-29 17:31:24 +0100

    Hide block when it is in wrong position, add physics interpolation

--- pieces/block.gd
+++ pieces/block.gd
@@ -4,18 +4,20 @@ class_name Block
 @onready var mesh := $Mesh as MeshInstance3D
 var is_ghost := false
 
-func _ready() -> void:
-	collision_mask = (2 << 31) - 1  # All layers
+func _init() -> void:
+	collision_layer = 0  # Do not affect physics
+	collision_mask = (2 << 31) - 1  # Scan all layers
+
 
 func set_ghost(ghost: bool) -> void:
 	is_ghost = ghost
 	if is_ghost:
 		var material := StandardMaterial3D.new()
 		material.cull_mode = BaseMaterial3D.CULL_DISABLED
-		material.blend_mode = BaseMaterial3D.BLEND_MODE_ADD  # does not cast shadows
+		material.blend_mode = BaseMaterial3D.BLEND_MODE_ADD  # Does not cast shadows
 		material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 		mesh.material_override = material
-		collision_layer = 0  # Do not affect physics
+		collision_layer = 0
 		max_contacts_reported = 1
 	else:
 		mesh.material_override = null
@@ -27,3 +29,14 @@ func set_ghost_color(color: Color) -> void:
 	if not is_ghost:
 		set_ghost(true)
 	mesh.material_override.albedo_color = color * 0.5
+
+
+func is_colliding() -> bool:
+	return get_contact_count() > 0
+
+
+func add_physics_interpolation() -> void:
+	var node := Node3D.new()
+	node.set_script(preload("res://physics_interpolation/node_3d.gd"))
+	add_child(node)
+	mesh.reparent(node)
--- player/builder.gd
+++ player/builder.gd
@@ -15,23 +15,37 @@ func _ready() -> void:
 
 
 func prepare_block() -> void:
-	block_instance = block_scenes[0].instantiate() as Block
-	player.add_child.call_deferred(block_instance)
+	var new_block_instance := block_scenes[0].instantiate() as Block
+	new_block_instance.hide()  # corrent position is set later in _process()
+	player.add_child.call_deferred(new_block_instance)
+	await new_block_instance.ready
+	block_instance = new_block_instance
 
 
 func _process(_delta: float) -> void:
-	force_raycast_update()
-	if is_colliding():
-		var point := get_collision_point()
-		block_instance.global_position = point + get_collision_normal() * 0.01
-		if block_instance.get_contact_count() == 0:
-			block_instance.set_ghost_color(Color.GREEN)
-			if Input.is_action_just_pressed("fire"):
-				block_instance.set_ghost(false)
-				block_instance.top_level = true
-				prepare_block()
+	if block_instance:
+		force_raycast_update()
+		if is_colliding():
+			var point := get_collision_point()
+			var normal := get_collision_normal()
+			block_instance.global_basis = Basis(
+				normal.cross(-normal.cross(player.global_basis.x)),
+				normal,
+				normal.cross(-normal.cross(player.global_basis.z))
+			).orthonormalized()
+			block_instance.global_position = point + normal * 0.001
+			block_instance.show()
+			if block_instance.is_colliding():
+				block_instance.set_ghost_color(Color.YELLOW)
+			else:
+				block_instance.set_ghost_color(Color.GREEN)
+				if Input.is_action_just_pressed("fire"):
+					block_instance.add_physics_interpolation()
+					block_instance.set_ghost(false)
+					if get_collider() is RigidBody3D:
+						block_instance.linear_velocity = get_collider().linear_velocity
+					prepare_block()
 		else:
-			block_instance.set_ghost_color(Color.YELLOW)
-	else:
-		block_instance.global_position = camera.global_position - camera.global_basis.z * 3.0
-		block_instance.set_ghost_color(Color.RED)
+			block_instance.global_basis = player.global_basis
+			block_instance.global_position = camera.global_position - camera.global_basis.z * 3.0
+			block_instance.set_ghost_color(Color.RED)
