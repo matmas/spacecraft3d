4524af422f3209591edbd91971c9e01a7a01afb6
Martin Riesz <riesz.martin@gmail.com>
2024-01-30 02:50:22 +0100

    Add center dot crosshair

---
+++ crosshair/crosshair.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@onready var center_dot := $CenterDot as Sprite2D
+
+func _ready() -> void:
+	_update_crosshair()
+	get_tree().root.size_changed.connect(_update_crosshair)
+
+
+func _update_crosshair() -> void:
+	var center := get_viewport().get_visible_rect().size * 0.5
+	center_dot.position = center
