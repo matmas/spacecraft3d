8742a9aa0f72358dbc1db0deacd08a9b137abaa0
Martin Riesz <riesz.martin@gmail.com>
2024-02-08 12:46:06 +0100

    Prevent opening project with (*) project modification marker

--- addons/controller_icons/plugin.gd
+++ addons/controller_icons/plugin.gd
@@ -7,7 +7,7 @@ func _enter_tree():
 	inspector_plugin = preload("res://addons/controller_icons/objects/ControllerIconEditorInspector.gd").new()
 	inspector_plugin.editor_interface = get_editor_interface()
 
-	add_autoload_singleton("ControllerIcons", "res://addons/controller_icons/ControllerIcons.gd")
+	#add_autoload_singleton("ControllerIcons", "res://addons/controller_icons/ControllerIcons.gd")
 	add_inspector_plugin(inspector_plugin)
 
 func _exit_tree():
