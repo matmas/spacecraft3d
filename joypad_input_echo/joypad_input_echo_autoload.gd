0b2f9722f97ff99e50484589a068d397a94bcc71
Martin Riesz <riesz.martin@gmail.com>
2024-02-10 16:43:20 +0100

    Fix input echo when releases do not come

--- joypad_input_echo/joypad_input_echo_autoload.gd
+++ joypad_input_echo/joypad_input_echo_autoload.gd
@@ -1,15 +1,13 @@
 extends Node
 # https://docs.godotengine.org/en/stable/tutorials/inputs/controllers_gamepads_joysticks.html#echo-events
 
+const REPEATABLE_ACTIONS = [&"ui_left", &"ui_right", &"ui_up", &"ui_down"]
 const DELAY_SEC = 0.6
 const RATE = 25  # repeats per second
 
-var held_event: InputEventAction = null
-var time_pressed_msec: int
 var delay_timer := Timer.new()
 var repeat_timer := Timer.new()
-
-const REPEATABLE_ACTIONS = [&"ui_left", &"ui_right", &"ui_up", &"ui_down"]
+var repeating_event: InputEventAction = null
 
 func _ready() -> void:
 	delay_timer.wait_time = DELAY_SEC
@@ -24,17 +22,15 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	for action in REPEATABLE_ACTIONS:
 		if event.is_action(action, true):
-			if event.is_released() and held_event and held_event.is_action(action, true):
-				_cancel_echo()
-			if event.is_pressed() and held_event == event:
-				return  # Our own echo events
 			if event.is_pressed():
-				# Setting event to held_event won't work for joysticks
+				if event == repeating_event:
+					return  # Ignore our own echo events here
+				# Setting received event to repeating_event won't work for joysticks
 				# because the UI controls won't register them
 				# since they produce events continuously compared to buttons
-				held_event = InputEventAction.new()
-				held_event.action = action
-				held_event.pressed = true
+				repeating_event = InputEventAction.new()
+				repeating_event.action = action
+				repeating_event.pressed = true
 				delay_timer.start()
 				repeat_timer.stop()
 			return
@@ -45,31 +41,34 @@ func _input(event: InputEvent) -> void:
 
 
 func _cancel_echo() -> void:
-	held_event = null
+	repeating_event = null
 	delay_timer.stop()
 	repeat_timer.stop()
 
 
 func _on_delay_timer_timeout() -> void:
-	if held_event:
+	if repeating_event:
 		_echo_held_button()
 		repeat_timer.start()
 
 
 func _on_repeat_timer_timeout() -> void:
-	if held_event:
-		_echo_held_button()
+	if repeating_event:
+		if Input.is_action_pressed(repeating_event.action):
+			_echo_held_button()
+		else:
+			repeat_timer.stop()
 	else:
 		repeat_timer.stop()
 
 
 func _echo_held_button() -> void:
-	Input.parse_input_event(held_event)
+	Input.parse_input_event(repeating_event)
 
 
 func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_APPLICATION_FOCUS_OUT:
-			held_event = null
+			repeating_event = null
 			delay_timer.stop()
 			repeat_timer.stop()
