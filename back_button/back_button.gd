ccb8c14448d13900b2fc3fed9ad7f24b67feb45c
Martin Riesz <riesz.martin@gmail.com>
2024-02-11 04:15:29 +0100

    Add back button to input editor and create game folder

---
+++ back_button/back_button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_pressed() -> void:
+	owner.get_parent().remove_child(owner)
+	owner.queue_free()
--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,15 +1,21 @@
 extends Control
 
+const INPUTMAP_EDITOR = preload("res://inputmap_editor/inputmap_editor.tscn")
+
 func _ready() -> void:
 	Utils.grab_focus_first_button(self)
 
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://game/game.tscn")
 
 
 func _on_controls_pressed() -> void:
-	get_tree().change_scene_to_file("res://inputmap_editor/inputmap_editor.tscn")
+	var button := get_window().gui_get_focus_owner()
+	var inputmap_editor := INPUTMAP_EDITOR.instantiate()
+	get_window().add_child(inputmap_editor)
+	hide()
+	inputmap_editor.tree_exited.connect(func(): show(); button.grab_focus())
 
 
 func _on_quit_pressed() -> void:
