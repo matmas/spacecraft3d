5d3b86d5e911185e360631e3d600a9ee4c0da5a0
Martin Riesz <riesz.martin@gmail.com>
2024-02-12 04:57:50 +0100

    Allow unpause with key action again

--- pause_menu/pause_menu.gd
+++ pause_menu/pause_menu.gd
@@ -1,19 +1,21 @@
 extends Control
 
 func _ready():
-	hide()
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	_set_paused(false)
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"pause_menu"):
-		get_tree().paused = true
-		show()
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		Utils.grab_focus_first_button(self)
+		_set_paused(not get_tree().paused)
 
 
 func _on_resume_pressed() -> void:
-	get_tree().paused = false
-	hide()
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	_set_paused(false)
+
+
+func _set_paused(paused: bool) -> void:
+	get_tree().paused = paused
+	visible = paused
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if paused else Input.MOUSE_MODE_CAPTURED
+	if visible:
+		Utils.grab_focus_first_button(self)
