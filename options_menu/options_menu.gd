1c6efc693018ff1bb59ee9f3beb1601eb18cb25c
Martin Riesz <riesz.martin@gmail.com>
2024-02-16 04:30:22 +0100

    Add Options and Display menus

---
+++ options_menu/options_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _ready() -> void:
+	Utils.grab_focus_first_button(self)
--- scene_buttons/scene_button.gd
+++ scene_buttons/scene_button.gd
@@ -2,9 +2,34 @@ extends Button
 
 @export var scene: PackedScene
 
+var _saved_shortcuts := {}
+
 func _on_pressed():
-	var button := get_window().gui_get_focus_owner()
 	var scene_instance := scene.instantiate()
 	get_window().add_child(scene_instance)
 	owner.hide()
-	scene_instance.tree_exited.connect(func(): owner.show(); button.grab_focus())
+	_clear_shortcuts_recursively(owner)
+	scene_instance.tree_exited.connect(_on_scene_closed)
+
+
+func _on_scene_closed() -> void:
+	owner.show()
+	_restore_shortcuts_recursively(owner)
+	grab_focus()
+
+
+func _clear_shortcuts_recursively(node: Node) -> void:
+	for child in node.get_children():
+		if child is BaseButton:
+			var button := child as BaseButton
+			_saved_shortcuts[child] = button.shortcut
+			button.shortcut = null
+		_clear_shortcuts_recursively(child)
+
+
+func _restore_shortcuts_recursively(node: Node) -> void:
+	for child in node.get_children():
+		if child is BaseButton:
+			var button := child as BaseButton
+			button.shortcut = _saved_shortcuts[button]
+		_restore_shortcuts_recursively(child)
