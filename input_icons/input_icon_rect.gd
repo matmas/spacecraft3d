10f7b757eb86925202c797e5ee95834061f56ebd
Martin Riesz <riesz.martin@gmail.com>
2024-02-16 17:29:54 +0100

    Add mouse icons from CC0 Xelu_Free_Controller&Key_Prompts

--- input_icons/input_icon_rect.gd
+++ input_icons/input_icon_rect.gd
@@ -6,7 +6,18 @@ class_name InputEventRect
 	set(value):
 		input_event = value
 		_update_text()
-		queue_redraw()
+
+var _texture: Texture
+
+func _draw() -> void:
+	if _texture:
+		draw_texture(_texture, Vector2())
+	else:
+		super._draw()
+
+
+func _set_texture(relative_path_without_extension: String) -> void:
+	_texture = ResourceLoader.load("res://input_icons/icons/%s.png" % relative_path_without_extension)
 
 
 func _validate_property(property: Dictionary) -> void:
@@ -26,5 +37,31 @@ func _update_text() -> void:
 						event.physical_keycode
 					)
 				text = OS.get_keycode_string(keycode)
+				_texture = null
+			"InputEventMouseButton":
+				var event := input_event as InputEventMouseButton
+				match event.button_index:
+					MOUSE_BUTTON_LEFT:  # Primary mouse button, usually assigned to the left button.
+						_set_texture("mouse/left")
+					MOUSE_BUTTON_RIGHT:  # Secondary mouse button, usually assigned to the right button.
+						_set_texture("mouse/right")
+					MOUSE_BUTTON_MIDDLE:
+						_set_texture("mouse/middle")
+					MOUSE_BUTTON_WHEEL_UP:
+						_set_texture("mouse/wheel_up")
+					MOUSE_BUTTON_WHEEL_DOWN:
+						_set_texture("mouse/wheel_down")
+					MOUSE_BUTTON_WHEEL_LEFT:
+						_set_texture("mouse/wheel_left")
+					MOUSE_BUTTON_WHEEL_RIGHT:
+						_set_texture("mouse/wheel_right")
+					MOUSE_BUTTON_XBUTTON1:  # Extra mouse button 1. This is sometimes present, usually to the sides of the mouse.
+						_set_texture("mouse/thumb_button_1")
+					MOUSE_BUTTON_XBUTTON2:  # Extra mouse button 2. This is sometimes present, usually to the sides of the mouse.
+						_set_texture("mouse/thumb_button_2")
+			"InputEventJoypadButton":
+				var event := input_event as InputEventJoypadButton
+				print(event.as_text())
 	else:
 		text = ""
+	queue_redraw()
