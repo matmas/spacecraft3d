eb73a4465bf2519faf2e7de66d6b10f90e706f42
Martin Riesz <riesz.martin@gmail.com>
2024-02-17 02:31:54 +0100

    Add Xbox Series controller icons

--- input_icons/input_icon_texture_rect.gd
+++ input_icons/input_icon_texture_rect.gd
@@ -27,10 +27,6 @@ func _validate_property(property: Dictionary) -> void:
 		property.usage = PROPERTY_USAGE_NONE
 
 
-func _set_texture(relative_path_without_extension: String) -> void:
-	texture = ResourceLoader.load("res://input_icons/icons/%s.png" % relative_path_without_extension)
-
-
 func _update_text() -> void:
 	if input_event:
 		match input_event.get_class():
@@ -47,26 +43,116 @@ func _update_text() -> void:
 				var event := input_event as InputEventMouseButton
 				match event.button_index:
 					MOUSE_BUTTON_LEFT:  # Primary mouse button, usually assigned to the left button.
-						_set_texture("mouse/left")
+						_set_texture("mouse", "left")
 					MOUSE_BUTTON_RIGHT:  # Secondary mouse button, usually assigned to the right button.
-						_set_texture("mouse/right")
+						_set_texture("mouse", "right")
 					MOUSE_BUTTON_MIDDLE:
-						_set_texture("mouse/middle")
+						_set_texture("mouse", "middle")
 					MOUSE_BUTTON_WHEEL_UP:
-						_set_texture("mouse/wheel_up")
+						_set_texture("mouse", "wheel_up")
 					MOUSE_BUTTON_WHEEL_DOWN:
-						_set_texture("mouse/wheel_down")
+						_set_texture("mouse", "wheel_down")
 					MOUSE_BUTTON_WHEEL_LEFT:
-						_set_texture("mouse/wheel_left")
+						_set_texture("mouse", "wheel_left")
 					MOUSE_BUTTON_WHEEL_RIGHT:
-						_set_texture("mouse/wheel_right")
+						_set_texture("mouse", "wheel_right")
 					MOUSE_BUTTON_XBUTTON1:  # Extra mouse button 1. This is sometimes present, usually to the sides of the mouse.
-						_set_texture("mouse/thumb_button_1")
+						_set_texture("mouse", "thumb_button_1")
 					MOUSE_BUTTON_XBUTTON2:  # Extra mouse button 2. This is sometimes present, usually to the sides of the mouse.
-						_set_texture("mouse/thumb_button_2")
+						_set_texture("mouse", "thumb_button_2")
+					_:
+						texture = null; text = ""
 			"InputEventJoypadButton":
 				var event := input_event as InputEventJoypadButton
-				print(event.as_text())
+				match event.button_index:
+					JOY_BUTTON_A:
+						_set_texture(_get_joypad_name(), "a")
+					JOY_BUTTON_B:
+						_set_texture(_get_joypad_name(), "b")
+					JOY_BUTTON_X:
+						_set_texture(_get_joypad_name(), "x")
+					JOY_BUTTON_Y:
+						_set_texture(_get_joypad_name(), "y")
+					JOY_BUTTON_BACK:
+						_set_texture(_get_joypad_name(), "back")
+					JOY_BUTTON_GUIDE:
+						texture = null; text = "Guide"
+					JOY_BUTTON_START:
+						_set_texture(_get_joypad_name(), "start")
+					JOY_BUTTON_LEFT_STICK:
+						_set_texture(_get_joypad_name(), "left_stick_click")
+					JOY_BUTTON_RIGHT_STICK:
+						_set_texture(_get_joypad_name(), "right_stick_click")
+					JOY_BUTTON_LEFT_SHOULDER:
+						_set_texture(_get_joypad_name(), "left_shoulder")
+					JOY_BUTTON_RIGHT_SHOULDER:
+						_set_texture(_get_joypad_name(), "right_shoulder")
+					JOY_BUTTON_DPAD_UP:
+						_set_texture(_get_joypad_name(), "dpad_up")
+					JOY_BUTTON_DPAD_DOWN:
+						_set_texture(_get_joypad_name(), "dpad_down")
+					JOY_BUTTON_DPAD_LEFT:
+						_set_texture(_get_joypad_name(), "dpad_left")
+					JOY_BUTTON_DPAD_RIGHT:
+						_set_texture(_get_joypad_name(), "dpad_right")
+					JOY_BUTTON_MISC1:
+						_set_texture(_get_joypad_name(), "misc")
+					JOY_BUTTON_PADDLE1:
+						texture = null; text = "Paddle 1"
+					JOY_BUTTON_PADDLE2:
+						texture = null; text = "Paddle 2"
+					JOY_BUTTON_PADDLE3:
+						texture = null; text = "Paddle 3"
+					JOY_BUTTON_PADDLE4:
+						texture = null; text = "Paddle 4"
+					JOY_BUTTON_TOUCHPAD:
+						_set_texture(_get_joypad_name(), "touchpad")
+					_:
+						texture = null; text = ""
+			"InputEventJoypadMotion":
+				var event := input_event as InputEventJoypadMotion
+				match event.axis:
+					JOY_AXIS_LEFT_X when event.axis_value > 0:
+						_set_texture(_get_joypad_name(), "left_stick_right")
+					JOY_AXIS_LEFT_X when event.axis_value < 0:
+						_set_texture(_get_joypad_name(), "left_stick_left")
+					JOY_AXIS_LEFT_Y when event.axis_value > 0:
+						_set_texture(_get_joypad_name(), "left_stick_down")
+					JOY_AXIS_LEFT_Y when event.axis_value < 0:
+						_set_texture(_get_joypad_name(), "left_stick_up")
+					JOY_AXIS_RIGHT_X when event.axis_value > 0:
+						_set_texture(_get_joypad_name(), "right_stick_right")
+					JOY_AXIS_RIGHT_X when event.axis_value < 0:
+						_set_texture(_get_joypad_name(), "right_stick_left")
+					JOY_AXIS_RIGHT_Y when event.axis_value > 0:
+						_set_texture(_get_joypad_name(), "right_stick_down")
+					JOY_AXIS_RIGHT_Y when event.axis_value < 0:
+						_set_texture(_get_joypad_name(), "right_stick_up")
+					JOY_AXIS_TRIGGER_LEFT:
+						_set_texture(_get_joypad_name(), "left_trigger")
+					JOY_AXIS_TRIGGER_RIGHT:
+						_set_texture(_get_joypad_name(), "right_trigger")
+					JOY_AXIS_LEFT_X, JOY_AXIS_LEFT_Y when event.axis_value == 0:
+						_set_texture(_get_joypad_name(), "left_stick")
+					JOY_AXIS_RIGHT_X, JOY_AXIS_RIGHT_Y when event.axis_value == 0:
+						_set_texture(_get_joypad_name(), "right_stick")
+					_:
+						texture = null; text = ""
 	else:
 		text = ""
 	queue_redraw()
+
+
+func _set_texture(folder: String, filename: String, extension: String = "png") -> void:
+	var base_path := "res://input_icons/icons"
+	var path := "%s/%s/%s.%s" % [base_path, folder, filename, extension]
+	if FileAccess.file_exists(path):
+		texture = ResourceLoader.load(path)
+	else:
+		texture = null; text = ""
+
+
+func _get_joypad_name() -> String:
+	#print(Input.get_joy_info(1))
+	#print(Input.get_joy_name(1))
+	return "xbox_series"
--- input_monitor/input_monitor_autoload.gd
+++ input_monitor/input_monitor_autoload.gd
@@ -1,10 +1,9 @@
 @tool
 extends Node
-# Adapted code from https://github.com/rsubtil/controller_icons by Ricardo Subtil, (c) 2023, MIT license
 
 signal input_type_changed(input_type)
 
-const MOUSE_MIN_MOVEMENT = 200
+const MOUSE_MIN_MOVEMENT = 100
 const JOYPAD_DEADZONE = 0.5
 
 enum InputType {
@@ -17,9 +16,13 @@ var current_input_type: InputType
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS  # Allow in paused game menus such as control remapping
 
-	if Input.get_connected_joypads():
-		_set_current_input_type(InputType.JOYPAD)
-	else:
+	var found_known_joypad := false
+	for device in Input.get_connected_joypads():
+		# Some joypads are not known, e.g. Wooting one keyboard
+		if Input.is_joy_known(device):
+			found_known_joypad = true
+			_set_current_input_type(InputType.JOYPAD)
+	if not found_known_joypad:
 		_set_current_input_type(InputType.KEYBOARD_AND_MOUSE)
 
 	Input.joy_connection_changed.connect(_on_joy_connection_changed)
@@ -32,23 +35,23 @@ func _on_joy_connection_changed(_device: int, connected: bool) -> void:
 		_set_current_input_type(InputType.KEYBOARD_AND_MOUSE)
 
 
-func _input(event: InputEvent):
-	var input_type = current_input_type
+func _input(event: InputEvent) -> void:
+	var new_input_type := current_input_type
 	match event.get_class():
 		"InputEventKey", "InputEventMouseButton":
-			input_type = InputType.KEYBOARD_AND_MOUSE
+			new_input_type = InputType.KEYBOARD_AND_MOUSE
 		"InputEventMouseMotion":
-			if event.velocity.length() > MOUSE_MIN_MOVEMENT:
-				input_type = InputType.KEYBOARD_AND_MOUSE
+			if (event as InputEventMouseMotion).velocity.length() > MOUSE_MIN_MOVEMENT:
+				new_input_type = InputType.KEYBOARD_AND_MOUSE
 		"InputEventJoypadButton":
-			input_type = InputType.JOYPAD
+			new_input_type = InputType.JOYPAD
 		"InputEventJoypadMotion":
-			if abs(event.axis_value) > JOYPAD_DEADZONE:
-				input_type = InputType.JOYPAD
-	if input_type != current_input_type:
-		_set_current_input_type(input_type)
+			if absf((event as InputEventJoypadMotion).axis_value) > JOYPAD_DEADZONE:
+				new_input_type = InputType.JOYPAD
+	if new_input_type != current_input_type:
+		_set_current_input_type(new_input_type)
 
 
-func _set_current_input_type(_current_input_type):
+func _set_current_input_type(_current_input_type: InputType) -> void:
 	current_input_type = _current_input_type
 	input_type_changed.emit(_current_input_type)
