a07c773ec7d1acf5155f436c1b187883e20a283c
Martin Riesz <riesz.martin@gmail.com>
2024-02-18 05:08:32 +0100

    Fix errors in stderr and change jump/crouch joypad keys

--- input_icons/input_utils_autoload.gd
+++ input_icons/input_utils_autoload.gd
@@ -5,9 +5,13 @@ extends Node
 func inputmap_get_events(action: StringName) -> Array[InputEvent]:
 	# Inside the editor InputMap.action_get_events() doesn't return custom events in ProjectSettings
 	if Engine.is_editor_hint():
-		var typed_array: Array[InputEvent] = []
-		typed_array.assign(ProjectSettings.get_setting("input/%s" % action)["events"])
-		return typed_array
+		var action_setting = ProjectSettings.get_setting("input/%s" % action)
+		if action_setting:
+			var typed_array: Array[InputEvent] = []
+			typed_array.assign(action_setting["events"])
+			return typed_array
+		else:
+			return []
 	else:
 		return InputMap.action_get_events(action)
 
