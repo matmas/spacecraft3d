b55c532a0c2e8e5256b9a4c1c65bf99433ac2f56
Martin Riesz <riesz.martin@gmail.com>
2024-02-21 17:48:01 +0100

    Reselect previous value on error

--- options_persistence/ui_components/option_button.gd
+++ options_persistence/ui_components/option_button.gd
@@ -8,6 +8,10 @@ func _init() -> void:
 
 
 func _ready() -> void:
+	_select_active()
+
+
+func _select_active() -> void:
 	for index in item_count:
 		var value := get_item_text(index)
 		if OptionsPersistence.get_handler(key, section).value_string_matches(value):
@@ -18,3 +22,4 @@ func _on_item_selected(index: int) -> void:
 	var value := get_item_text(index)
 	OptionsPersistence.get_handler(key, section).set_value_string(value)
 	OptionsPersistence.save()
+	_select_active()  # If something goes wrong we revert back to previous value
