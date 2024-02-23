88dbd03285962ac760062962a08a9c3a393729c0
Martin Riesz <riesz.martin@gmail.com>
2024-02-23 16:16:18 +0100

    Let features use signals instead of checking every frame

--- game_options/feature.gd
+++ game_options/feature.gd
@@ -9,7 +9,12 @@ func _init() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 
-func _process(_delta: float) -> void:
+func _ready() -> void:
+	_update()
+	GameOptions.get_option(section, key).value_changed.connect(func(_value): _update())
+
+
+func _update() -> void:
 	var parent := get_parent()
 	parent.visible = GameOptions.get_option(section, key).get_value()
 	parent.set_process(parent.visible)
