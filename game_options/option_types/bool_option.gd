f5fda44a3f3ddf91cdaf2bb7e52a2d2d4cf715aa
Martin Riesz <riesz.martin@gmail.com>
2024-02-23 13:10:54 +0100

    Reduce BoolOption to just a marker class

--- game_options/option_types/bool_option.gd
+++ game_options/option_types/bool_option.gd
@@ -1,19 +1,2 @@
 extends EnumOption
 class_name BoolOption
-
-var ENABLED := tr("Enabled")
-var DISABLED := tr("Disabled")
-
-
-func get_value_from_string(value: String) -> Variant:
-	match value:
-		DISABLED:
-			return false
-		ENABLED:
-			return true
-		_:
-			return null
-
-
-func get_possible_string_values() -> Array[String]:
-	return [DISABLED, ENABLED]
