b4e5324de79ff4dafc26a5d04fbf9151b0fdad0a
Martin Riesz <riesz.martin@gmail.com>
2024-02-25 04:09:42 +0100

    Reduce FPS when window is unfocused

--- game_options/options/enum/max_fps.gd
+++ game_options/options/enum/max_fps.gd
@@ -1,6 +1,9 @@
 extends EnumOption
 
+const UNFOCUSED_WINDOW_FPS = 4
 var UNLIMITED = tr("Unlimited")
+
+var current_value := Engine.max_fps
 
 
 func section() -> String:
@@ -21,11 +24,12 @@ func get_display_category() -> String:
 
 func set_value(value: Variant) -> void:
 	Engine.max_fps = value
+	current_value = value
 	value_changed.emit()
 
 
 func get_value() -> Variant:
-	return Engine.max_fps
+	return current_value
 
 
 func get_value_from_display_value(value: String) -> Variant:
@@ -36,3 +40,13 @@ func get_value_from_display_value(value: String) -> Variant:
 
 func get_possible_display_values() -> Array[String]:
 	return ["15", "30", "40", "60", "75", "90", "120", "144", "160", "165", "175", "180", "200", "240", "250", "300", "360", "500", UNLIMITED]
+
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_APPLICATION_FOCUS_OUT:
+			Engine.max_fps = UNFOCUSED_WINDOW_FPS
+			OS.low_processor_usage_mode = true
+		NOTIFICATION_APPLICATION_FOCUS_IN:
+			Engine.max_fps = current_value
+			OS.low_processor_usage_mode = false
