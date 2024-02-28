b9da539d5e090b5e288cebd9010dec0e18f7befd
Martin Riesz <riesz.martin@gmail.com>
2024-02-28 14:56:22 +0100

    Fix flow visible initially despite being turned off in options

--- game_options/options/graphics/effects/glow.gd
+++ game_options/options/graphics/effects/glow.gd
@@ -15,14 +15,9 @@ func set_value(value: bool) -> void:
 	var camera := get_viewport().get_camera_3d()
 	if camera:
 		camera.get_world_3d().environment.glow_enabled = value
-	else:
-		current_value = value
+	current_value = value
 	value_changed.emit()
 
 
 func get_value() -> bool:
-	var camera := get_viewport().get_camera_3d()
-	if camera:
-		return camera.get_world_3d().environment.glow_enabled
-	else:
-		return current_value
+	return current_value
