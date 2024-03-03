103c257e7607d1b0a81b29f85ff4396b4bdb766b
Martin Riesz <riesz.martin@gmail.com>
2024-03-03 12:28:02 +0100

    Save slider option also when using keyboard or controller

--- game_options/ui_components/slider.gd
+++ game_options/ui_components/slider.gd
@@ -3,14 +3,17 @@ class_name GameOptionSlider
 
 var option: Option
 
+var _is_dragging := false
+
 
 func _ready() -> void:
 	min_value = option.get_min_value()
 	max_value = option.get_max_value()
 	step = option.get_step()
 
-	# After setting min_value, max_value as those could emit value_changed
+	# Connect signals after setting min_value, max_value as those could emit value_changed
 	value_changed.connect(_on_value_changed)
+	drag_started.connect(_on_drag_started)
 	drag_ended.connect(_on_drag_ended)
 
 	_refresh()
@@ -19,9 +22,16 @@ func _ready() -> void:
 
 func _on_value_changed(_value: float) -> void:
 	option.set_value(value)
+	if not _is_dragging:  # Using keyboard or controller
+		GameOptions.save()
+
+
+func _on_drag_started() -> void:
+	_is_dragging = true
 
 
 func _on_drag_ended(_value_changed: bool) -> void:
+	_is_dragging = false
 	if _value_changed:
 		GameOptions.save()
 
