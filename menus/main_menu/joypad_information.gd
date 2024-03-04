902633a4a60877f4d951f4e322bfa0b53f08a5e9
Martin Riesz <riesz.martin@gmail.com>
2024-03-04 01:10:45 +0100

    Add joypad information to main menu

---
+++ menus/main_menu/joypad_information.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Input.get_connected_joypads():
+		text = "Detected controllers:\n"
+
+	for device in Input.get_connected_joypads():
+		var joy_info := Input.get_joy_info(device)
+		var vendor_id: int = joy_info["vendor_id"]
+		var product_id: int = joy_info["product_id"]
+		var raw_name: String = joy_info["raw_name"]
+		text += "%04x-%04x-%s%s\n" % [vendor_id, product_id, raw_name, "*" if Input.is_joy_known(device) else ""]
