739c846801c47d692b7d65fe522de43f3faac0d6
Martin Riesz <riesz.martin@gmail.com>
2024-03-04 01:24:38 +0100

    Move joypad information to credits scene

--- menus/credits/joypad_information.gd
+++ menus/credits/joypad_information.gd
@@ -3,8 +3,8 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Input.get_connected_joypads():
-		text = "Detected controllers:\n"
+	if Input.get_connected_joypads().size():
+		text = "Detected controllers: %s\n" % Input.get_connected_joypads().size()
 
 	for device in Input.get_connected_joypads():
 		var joy_info := Input.get_joy_info(device)
@@ -12,3 +12,6 @@ func _ready() -> void:
 		var product_id: int = joy_info["product_id"]
 		var raw_name: String = joy_info["raw_name"]
 		text += "%04x-%04x-%s%s\n" % [vendor_id, product_id, raw_name, "*" if Input.is_joy_known(device) else ""]
+
+#045e-0b12-Microsoft Xbox Series S|X Controller*
+#03eb-ff01-Wooting One (Legacy)
