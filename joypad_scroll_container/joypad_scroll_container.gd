e09282a497166ab85e41ef700ba6821d6b18ca4e
Martin Riesz <riesz.martin@gmail.com>
2024-03-05 15:57:43 +0100

    Rename variable with _ prefix

--- joypad_scroll_container/joypad_scroll_container.gd
+++ joypad_scroll_container/joypad_scroll_container.gd
@@ -3,19 +3,19 @@ class_name JoypadScrollContainer
 
 const SCROLL_SPEED = 750
 
-var scroll_accumulator := Vector2()
+var _scroll_accumulator := Vector2()
 
 
 func _process(delta: float) -> void:
-	scroll_accumulator += Input.get_vector(
+	_scroll_accumulator += Input.get_vector(
 		&"ui_scroll_left", &"ui_scroll_right", &"ui_scroll_up", &"ui_scroll_down"
 	) * SCROLL_SPEED * delta
 
-	if absf(scroll_accumulator.x) > 1:
-		var scroll_int := int(scroll_accumulator.x)
+	if absf(_scroll_accumulator.x) > 1:
+		var scroll_int := int(_scroll_accumulator.x)
 		scroll_horizontal += scroll_int
-		scroll_accumulator.x -= scroll_int
-	if absf(scroll_accumulator.y) > 1:
-		var scroll_int := int(scroll_accumulator.y)
+		_scroll_accumulator.x -= scroll_int
+	if absf(_scroll_accumulator.y) > 1:
+		var scroll_int := int(_scroll_accumulator.y)
 		scroll_vertical += scroll_int
-		scroll_accumulator.y -= scroll_int
+		_scroll_accumulator.y -= scroll_int
