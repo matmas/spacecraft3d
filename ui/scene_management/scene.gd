972f4e79116899a404738f91bb325b14bedfcda5
Martin Riesz <riesz.martin@gmail.com>
2024-03-11 03:43:00 +0100

    Add @export var Scene.close_scene_action

--- ui/scene_management/scene.gd
+++ ui/scene_management/scene.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name Scene
 
+@export var close_scene_action := &""
+
 
 func _ready() -> void:
 	_refresh()
@@ -41,3 +43,10 @@ func should_hide_mouse_cursor() -> bool:
 
 func on_go_back_requested() -> void:
 	SceneManagement.close_current_scene()
+
+
+func _input(event: InputEvent) -> void:
+	if close_scene_action:
+		if SceneManagement.current_scene() == self and event.is_action_pressed(close_scene_action):
+			get_viewport().set_input_as_handled()
+			SceneManagement.close_current_scene()
