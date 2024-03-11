449a5203208ac1749cc9815b7a69cc6152210f1c
Martin Riesz <riesz.martin@gmail.com>
2024-03-11 02:17:33 +0100

    Close build_menu when <Tab> is pressed again

--- building_mechanics/build_menu/build_menu.gd
+++ building_mechanics/build_menu/build_menu.gd
@@ -4,3 +4,9 @@ class_name BuildMenu
 
 func should_pause_game() -> bool:
 	return false
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"build_menu"):  # We can't use Button.shortcut as it clashes with ui_focus_next which takes priority
+		get_viewport().set_input_as_handled()
+		SceneManagement.close_current_scene()
