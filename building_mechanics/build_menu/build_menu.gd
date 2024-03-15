fd57a2230653584f39c3cbea14d414ab1bf5cd27
Martin Riesz <riesz.martin@gmail.com>
2024-03-15 11:37:30 +0100

    Use PieceLibrary information to instantiate build menu scene viewers

--- building_mechanics/build_menu/build_menu.gd
+++ building_mechanics/build_menu/build_menu.gd
@@ -1,6 +1,18 @@
 extends Scene
 class_name BuildMenu
 
+const SCENE_VIEWER_SCENE = preload("scene_viewer/scene_viewer.tscn")
+
+@onready var flow_container: HFlowContainer = %HFlowContainer
+
+
+func _ready() -> void:
+	for scene in PieceLibrary.piece_scenes:
+		var scene_viewer := SCENE_VIEWER_SCENE.instantiate()
+		scene_viewer.scene = scene
+		flow_container.add_child(scene_viewer)
+	super._ready()
+
 
 func should_pause_game() -> bool:
 	return false
--- building_mechanics/build_menu/scene_viewer/scene_viewer.gd
+++ building_mechanics/build_menu/scene_viewer/scene_viewer.gd
@@ -1,5 +1,6 @@
 @tool
 extends Control
+class_name SceneViewer
 
 @export var scene: PackedScene:
 	set(value):
