59eefc8dd6a38790800e0f8dca988f8337737d23
Martin Riesz <riesz.martin@gmail.com>
2024-05-02 04:29:48 +0200

    Convert into addon joypad_input_echo

---
+++ addons/joypad_input_echo/plugin.gd
@@ -0,0 +1,15 @@
+@tool
+extends EditorPlugin
+
+const AUTOLOAD_SINGLETON_NAME = "JoypadInputEcho"
+
+
+func _enter_tree() -> void:
+	await get_tree().process_frame  # Wait for @tool autoloads to load
+
+	if not get_node_or_null("/root/%s" % AUTOLOAD_SINGLETON_NAME):  # Avoid project modification indicator in the Godot window title (*) everytime project loads
+		add_autoload_singleton(AUTOLOAD_SINGLETON_NAME, "autoload.gd")
+
+
+func _exit_tree() -> void:
+	remove_autoload_singleton(AUTOLOAD_SINGLETON_NAME)
