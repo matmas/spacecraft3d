f6d145c41e95f2e385b859832672aa059db25d8c
Martin Riesz <riesz.martin@gmail.com>
2024-05-04 13:32:36 +0200

    Hide select_object input hint when game is not current_scene

--- ui/object_selection/object_selection.gd
+++ ui/object_selection/object_selection.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	if InputHints.is_action_just_pressed(&"select_object"):
+	if SceneManagement.current_scene() is Game and InputHints.is_action_just_pressed(&"select_object"):
 		var camera := get_viewport().get_camera_3d()
 		var params := PhysicsRayQueryParameters3D.new()
 		params.from = camera.project_ray_origin(get_viewport().get_mouse_position())
