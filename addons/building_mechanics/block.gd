c06a3e3564ec1551deebcab5c21dce7629ba608b
Martin Riesz <riesz.martin@gmail.com>
2024-05-19 06:02:08 +0200

    Add tracking of block neighbors in a grid

--- addons/building_mechanics/block.gd
+++ addons/building_mechanics/block.gd
@@ -5,6 +5,7 @@ class_name Block
 @export_range(0.001, 1000, 0.001, "or_greater", "exp", "suffix:kg") var mass := 1.0
 
 var grid: Grid
+var neighbors: Array[Block] = []  # Touching blocks
 
 
 func _to_string() -> String:
@@ -18,3 +19,23 @@ func _notification(what: int) -> void:
 				grid = get_parent() as Grid
 		NOTIFICATION_EXIT_TREE:
 			grid = null
+			for neighbor in neighbors:
+				neighbor.neighbors.erase(self)
+
+
+func _ready() -> void:
+	if grid:
+		for neighbor in _get_neighbors_in_grid():
+			neighbor.neighbors.append(self)
+			neighbors.append(neighbor)
+
+
+func _get_neighbors_in_grid() -> Array[Block]:
+	var result: Array[Block] = []
+	for direction in [Vector3.LEFT, Vector3.RIGHT, Vector3.UP, Vector3.DOWN, Vector3.BACK, Vector3.FORWARD]:
+		for collider in BuildingMechanicsUtils.get_colliders_of_physics_body(self, grid.block_collision_mask, -0.02, self.basis * direction * 0.04):
+				if collider is Block:
+					var neighbor := collider as Block
+					if neighbor.grid == grid:
+						result.append(neighbor)
+	return result
