e940d7bd6342418c66623ffe0cc1b4d6cbb7afba
Martin Riesz <riesz.martin@gmail.com>
2024-07-09 13:54:01 +0200

    Fix match velocity to deleted grid

--- ui/object_selection/object_selection_autoload.gd
+++ ui/object_selection/object_selection_autoload.gd
@@ -1,4 +1,8 @@
 extends Node
 
 
-var selected_collider: Node3D  # Most likely CollisionObject3D (or VoxelLodTerrain)
+var selected_collider: Node3D:  # Most likely CollisionObject3D (or VoxelLodTerrain)
+	get:
+		if is_instance_valid(selected_collider):
+			return selected_collider
+		return null
