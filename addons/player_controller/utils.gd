de6fe76682153d142aea383fdb1637ce6ec6ea10
Martin Riesz <riesz.martin@gmail.com>
2024-11-02 04:34:34 +0100

    Revert "Dynamic player controller with smooth rotation"
    This reverts commit 38a34838c90b111c9665a1791ed995b4f8eecc68.
    
    Manual and built-in physics interpolation don't work well at higher speeds.

--- addons/player_controller/utils.gd
+++ addons/player_controller/utils.gd
@@ -12,22 +12,3 @@ static func get_velocity(node: Node3D) -> Vector3:
 	if node is RigidBody3D:
 		return (node as RigidBody3D).linear_velocity
 	return Vector3.ZERO
-
-
-static func transfer_basis_by_fraction(source: Node3D, target: PhysicsDirectBodyState3D, fraction: float) -> void:
-	var basis_delta := Basis.IDENTITY.slerp(source.transform.basis.orthonormalized(), fraction)
-	target.transform.basis *= basis_delta
-	source.transform.basis *= basis_delta.inverse()
-
-
-static func transfer_basis_to_euler_xz_by_fraction(source: PhysicsDirectBodyState3D, target_x: Node3D, target_z: Node3D, target_basis: Basis, fraction: float) -> void:
-	var euler := (target_basis.inverse() * source.transform.basis).get_euler()
-	var basis_delta_x := Basis.IDENTITY.slerp(Basis.from_euler(Vector3(euler.x, 0, 0)), fraction)
-	var basis_delta_z := Basis.IDENTITY.slerp(Basis.from_euler(Vector3(0, 0, euler.z)), fraction)
-	target_x.transform.basis *= basis_delta_x
-	target_z.transform.basis *= basis_delta_z
-	source.transform.basis *= basis_delta_x.inverse() * basis_delta_z.inverse()
-
-
-static func reset_basis_by_fraction(node: Node3D, fraction: float) -> void:
-	node.basis = node.basis.orthonormalized().slerp(Basis.IDENTITY, fraction).orthonormalized()
--- vfx/motion_particles/motion_particles.gd
+++ vfx/motion_particles/motion_particles.gd
@@ -7,7 +7,8 @@ var previous_camera_position: Vector3
 func _physics_process(delta: float) -> void:
 	var camera := get_viewport().get_camera_3d()
 	if camera:
-		global_transform = camera.global_transform
+		if get_parent_node_3d() != camera:
+			reparent(camera)
 
 		var camera_velocity := (camera.global_position - previous_camera_position) / delta
 		particles_material.set_shader_parameter(&"camera_velocity", camera_velocity)
